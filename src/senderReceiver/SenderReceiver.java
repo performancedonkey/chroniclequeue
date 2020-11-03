976c7925e86e6b6142cf0d6820bfc8e211bdaefb
Performance Donkey <performancedonkey@gmail.com>
2020-11-03 22:55:37 +0200

    introduce methods

--- src/senderReceiver/SenderReceiver.java
+++ src/senderReceiver/SenderReceiver.java
@@ -1,10 +1,12 @@
 package senderReceiver;
+
 import utils.NanoClock;
 import utils.NonBlockingSocket;
 import utils.SelectNetDriver;
 import org.apache.log4j.Logger;
 import setaffinitymanager.SetAffinityManager;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -40,12 +42,12 @@ public class SenderReceiver {
                 log.fatal("There must be at least 5 args: <-s/-r> <IP> <Port> <InterfaceName> [SleepMillis] "); // [TTLSeconds]
                 return;
             }
-            int sleep = Integer.valueOf(args[4]);
+            int sleep = Integer.parseInt(args[4]);
 
             java.security.Security.setProperty("networkaddress.cache.ttl", "30");
 
             String ip = args[1];
-            int port = Integer.valueOf(args[2]);
+            int port = Integer.parseInt(args[2]);
             String interfaceName = args[3];
 
             if (!args[0].equals("-r") && !args[0].equals("-s")) {
@@ -53,7 +55,7 @@ public class SenderReceiver {
                 return;
             }
 
-            packetLength = args.length > 5 ? Integer.valueOf(args[5]) : 12; // long + int
+            packetLength = args.length > 5 ? Integer.parseInt(args[5]) : 12; // long + int
             buffer = ByteBuffer.allocate(packetLength);
             buffer.order(ByteOrder.LITTLE_ENDIAN);
 
@@ -62,7 +64,6 @@ public class SenderReceiver {
                 new SenderReceiver().doSenderCode(ip, port, interfaceName, sleep);
             else new SenderReceiver().doReceiverCode(ip, port, interfaceName);
 
-
         } catch (Exception e) {
             log.error("Fatal error occurred!", e);
         } finally {
@@ -70,30 +71,22 @@ public class SenderReceiver {
         }
     }
 
+    int packetSeq = 0;
+
     private void doSenderCode(String ip, int port, String interfaceName, int sleep) {
         log.info("Doing sender code on: " + ip + " / " + port + " / " + interfaceName + " / " + sleep);
-        int packetSeq = 0;
-        MulticastSocket senderSocket = null;
-        try {
-            senderSocket = new MulticastSocket();
+        try (MulticastSocket senderSocket = new MulticastSocket()) {
             InetAddress group = InetAddress.getByName(ip);
             byte[] buf = buffer.array();
             DatagramPacket datagram = new DatagramPacket(buf, buf.length, group, port);
 
             senderSocket.setTimeToLive(30);
-            senderSocket.setInterface(InetAddress.getByName(interfaceName));
+            InetAddress address = InetAddress.getByName(interfaceName);
+            NetworkInterface nif = NetworkInterface.getByInetAddress(address);
+            senderSocket.setNetworkInterface(nif);
 
             while (running) {
-                buffer.clear();
-                buffer.putInt(0, ++packetSeq);
-                long sendingTime = NanoClock.getNanoTimeNow();
-                buffer.putLong(4, sendingTime);
-
-                datagram.setData(buffer.array(), 0, packetLength);
-
-                senderSocket.send(datagram);
-
-                packetSeq %= resetEvery;
+                long sendingTime = sendPacket(senderSocket, datagram);
 
                 log.info(String.format("Sending packet %d @ %d", packetSeq, sendingTime));
 
@@ -102,16 +95,31 @@ public class SenderReceiver {
         } catch (Exception e) {
             System.out.println("Error in Sender code: " + e);
         } finally {
-            if (senderSocket != null) senderSocket.close();
             System.out.println("Stopped sending");
         }
+    }
+
+    private long sendPacket(MulticastSocket senderSocket, DatagramPacket datagram) throws IOException {
+        long sendingTime = NanoClock.getNanoTimeNow();
+
+        buffer.clear();
+        buffer.putInt(0, ++packetSeq);
+        buffer.putLong(4, sendingTime);
+
+        datagram.setData(buffer.array(), 0, packetLength);
+
+        senderSocket.send(datagram);
+
+        packetSeq %= resetEvery;
+
+        return sendingTime;
     }
 
     boolean running = true;
     long lowestRead = Long.MAX_VALUE;
     long lowest = Long.MAX_VALUE;
     int expectedPacketSeq = 0;
-    HashSet<Integer> missedPacketSeqs = new HashSet<Integer>();
+    HashSet<Integer> missedPacketSeqs = new HashSet<>();
 
     private void doReceiverCode(String ip, int port, String interfaceName) {
         log.info("Doing receiver code on: " + ip + " / " + port + " / " + interfaceName);
